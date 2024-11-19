90e7600ce5ec75b60ebc48cf036a79ad34dcd5ff
Gladimore <105724638+Gladimore@users.noreply.github.com>
2024-11-19 00:41:16 -0600

    Delete api directory

--- api/utils.js
+++
@@ -1,9 +0,0 @@
-const fs = require("fs");
-const path = require("path");
-
-function GetFile(filePath) {
-  const fullPath = path.join(process.cwd(), filePath);
-  return fs.readFileSync(fullPath, "utf-8"); // Return the file as a string
-}
-
-module.exports = { GetFile };
