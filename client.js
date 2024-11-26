1c2eab8622de7d12e1e5cdffd36e93effa5135b9
Gladimore <105724638+Gladimore@users.noreply.github.com>
2024-11-26 16:30:47 -0600

    Update client.js

--- client.js
+++ client.js
@@ -6,7 +6,7 @@ class TogetherClient {
     this.together = new Together({
       apiKey: this.apiKey,
     });
-    this.default_tokens = 1024;
+    this.default_tokens = null;
   }
 
   async chat(model, messages, { max_tokens, ...rest } = {}) {
@@ -18,7 +18,7 @@ class TogetherClient {
       model: model,
       messages: messages,
       stream: false,
-      max_tokens: max_tokens == undefined? max_tokens: this.default_tokens,
+      max_tokens: max_tokens || this.default_tokens,
       ...rest,
     });
 
