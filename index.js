46fe1aace7bca2fadc27d27f03a4e37446f6c175
Gladimore <105724638+Gladimore@users.noreply.github.com>
2025-01-14 05:39:42 -0600

    Update index.js

--- index.js
+++ index.js
@@ -12,6 +12,10 @@ const PORT = process.env.PORT || 3000;
 
 app.use("/api", routes);
 
+app.get("*", (_, res) => {
+  res.sendStatus(404)
+})
+
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
